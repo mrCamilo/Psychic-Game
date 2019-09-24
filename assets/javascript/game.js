19d6b9ed568850235169cdce339d886033b6db42
Manuel Camilo <mcamilo@protonmail.com>
2019-09-24 19:48:51 -0400

    .

--- assets/javascript/game.js
+++ assets/javascript/game.js
@@ -1,31 +1,35 @@
- // Directions!
- document.getElementById("directions").innerHTML = "Guess what letter I'm thinking of... type any letter from a-z! You get 5 guesses!";
+    // Directions!
+    document.getElementById("directions").innerHTML = "Guess what letter I'm thinking of... type any letter from a-z! You get 9 guesses!";
 
- document.onkeyup=function(event) {
-        // variables to store wins, ties, losses, and onkeyup
+    // variables start off at 0 wins and losses
+    var wins = 0;
+    var losses = 0;
+
+    // array of all letters
+    var alphabet = ['a', 'b', 'c', 'd', 'e', 'f', 'g', 'h', 'i', 'j', 'k', 'l' , 'm', 'n', 'o', 'p', 'q', 'r', 's', 't', 'u', 'v', 'w', 'x', 'y', 'z'];
+    // Random letter
+    var randomComputerChoice = alphabet[Math.floor(Math.random() * alphabet.length)];
+
+     document.onkeyup=function(event) {      
         var userChoice = event.key;
-        var wins = 0;
-        var ties = 0;
-        var losses = 0;
         
-        // array of all letters
-        var alphabet = ['a', 'b', 'c', 'd', 'e', 'f', 'g', 'h', 'i', 'j', 'k', 'l' , 'm', 'n', 'o', 'p', 'q', 'r', 's', 't', 'u', 'v', 'w', 'x', 'y', 'z'];
-        // Random letter
-        var randomComputerChoice = alphabet[Math.floor(Math.random() * alphabet.length)];
-
+        
+        
         // run through the game
-        for (var numGuesses = 5; numGuesses >= 0; numGuesses--) {
-            if (randomComputerChoice == userChoice)
+        // for (var numGuesses = 5; numGuesses >= 0; numGuesses--) {
+            if (randomComputerChoice != userChoice)
             {
-                document.getElementById("winText").innerHTML = "You win!";
-                break;
-                
+                document.getElementById("guessesText").innerHTML = "Tries remaining: ";
+                wins ++;
+                // numGuesses++; // count number of guesses
             }
             else {
-                document.getElementById("guessesText").innerHTML = "Tries remaining: " + numGuesses; // count number of guesses
+                 
                 // document.getElementById("lossText").innerHTML = "You lose!";
+                document.getElementById("winText").innerHTML = "You win!";
+                break;
             }
-        }
+     // }
 
     }
 

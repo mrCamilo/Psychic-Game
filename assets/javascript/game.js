1e497d5afdb5b2525dbeab611e0f5a76c83b5fb1
Manuel Camilo <mcamilo@protonmail.com>
2019-09-24 18:48:07 -0400

    .

--- assets/javascript/game.js
+++ assets/javascript/game.js
@@ -1,23 +1,31 @@
  // Directions!
- document.getElementById("directions").innerHTML = "Guess what letter I'm thinking of... type any letter from a-z!";
+ document.getElementById("directions").innerHTML = "Guess what letter I'm thinking of... type any letter from a-z! You get 5 guesses!";
 
  document.onkeyup=function(event) {
-        // more variables to store wins, ties, losses
+        // variables to store wins, ties, losses, and onkeyup
+        var userChoice = event.key;
         var wins = 0;
         var ties = 0;
         var losses = 0;
+        
         // array of all letters
         var alphabet = ['a', 'b', 'c', 'd', 'e', 'f', 'g', 'h', 'i', 'j', 'k', 'l' , 'm', 'n', 'o', 'p', 'q', 'r', 's', 't', 'u', 'v', 'w', 'x', 'y', 'z'];
         // Random letter
         var randomComputerChoice = alphabet[Math.floor(Math.random() * alphabet.length)];
-        document.write(randomComputerChoice);
 
-        if(randomComputerChoice == this.onkeyup)
-        {
-            document.write("You win!")
+        // run through the game
+        for (var numGuesses = 5; numGuesses >= 0; numGuesses--) {
+            if (randomComputerChoice == userChoice)
+            {
+                document.getElementById("winText").innerHTML = "You win!";
+                break;
+                
+            }
+            else {
+                document.getElementById("guessesText").innerHTML = "Tries remaining: " + numGuesses; // count number of guesses
+                // document.getElementById("lossText").innerHTML = "You lose!";
+            }
         }
-        else {
-            document.write("You lose!");
-        }
- }
+
+    }
 

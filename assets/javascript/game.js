3c23204a432bf1c5ccf971d4a67e53262be2aac4
Manuel Camilo <mcamilo@protonmail.com>
2019-09-24 19:59:10 -0400

    .

--- assets/javascript/game.js
+++ assets/javascript/game.js
@@ -4,30 +4,29 @@
     // variables start off at 0 wins and losses
     var wins = 0;
     var losses = 0;
+    var tries = 9;
 
-    // array of all letters
+    // Computer picks from this array of letters
     var alphabet = ['a', 'b', 'c', 'd', 'e', 'f', 'g', 'h', 'i', 'j', 'k', 'l' , 'm', 'n', 'o', 'p', 'q', 'r', 's', 't', 'u', 'v', 'w', 'x', 'y', 'z'];
-    // Random letter
     var randomComputerChoice = alphabet[Math.floor(Math.random() * alphabet.length)];
 
+    // when user types a key...
      document.onkeyup=function(event) {      
-        var userChoice = event.key;
-        
-        
-        
+        var userChoice = event.key; // user input is saved as var userChoice
+          
         // run through the game
         // for (var numGuesses = 5; numGuesses >= 0; numGuesses--) {
             if (randomComputerChoice != userChoice)
             {
-                document.getElementById("guessesText").innerHTML = "Tries remaining: ";
-                wins ++;
-                // numGuesses++; // count number of guesses
+                document.getElementById("guessesText").innerHTML = "Tries remaining: " + tries;
+                tries -=1;
+                numGuesses++; // count number of guesses
             }
             else {
                  
                 // document.getElementById("lossText").innerHTML = "You lose!";
                 document.getElementById("winText").innerHTML = "You win!";
-                break;
+                document.write("epic");
             }
      // }
 
